2497460ad564da80302bbdf7d84f0f296f765114
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2017-12-17 23:22:11 +0100

    Cleaned microcontroller firmware initialization sequence, started control loop implementation.

--- Software/Microcontroller_Firmware/Sources/Main.c
+++ Software/Microcontroller_Firmware/Sources/Main.c
@@ -6,8 +6,10 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <Led.h>
+#include <Mixing_Valve.h>
 #include <Protocol.h>
 #include <Relay.h>
+#include <Temperature.h>
 #include <util/delay.h>
 
 //-------------------------------------------------------------------------------------------------
@@ -21,17 +23,26 @@ FUSES =
 	FUSE_BODLEVEL2 // Fuses extended byte : set brown-out reset voltage to approximately 4.3V
 };
 
+/** External sensor temperature (in °C). */
+static signed char Main_Temperature_Outside = 0;
+/** The pipe going to the radiators' temperature. */
+static signed char Main_Temperature_Radiator_Start = 0;
+/** The pipe coming from the radiators' temperature. */
+static signed char Main_Temperature_Radiator_Return = 0;
+
 //-------------------------------------------------------------------------------------------------
 // Entry point
 //-------------------------------------------------------------------------------------------------
 int main(void) // Can't use void return type because it triggers a warning
 {
-	unsigned char Is_WiFi_Successfully_Initialized, a = 1;
+	unsigned char Is_WiFi_Successfully_Initialized, Is_Status_Led_On = 1;
 	
 	// Initialize modules
 	LedInitialize();
+	LedTurnOn(LED_ID_STATUS); // Turn status led on to tell controller is booting
 	ADCInitialize();
 	RelayInitialize();
+	MixingValveInitialize();
 	Is_WiFi_Successfully_Initialized = ProtocolInitialize();
 	
 	// Enable interrupts now that all modules have been configured
@@ -40,23 +51,35 @@ int main(void) // Can't use void return type because it triggers a warning
 	// Tell whether network is working
 	if (!Is_WiFi_Successfully_Initialized) LedTurnOn(LED_ID_NETWORK_ERROR);
 	
+	// Start pump
+	RelayTurnOn(RELAY_ID_PUMP);
+	
 	while (1)
 	{
-		ADCTask(); // TODO execute this every second but without blocking all other tasks
+		// Sample all analog values
+		ADCTask();
 		
-		// TEST
-		_delay_ms(1000);
+		// Cache converted temperature values (conversion computations are cost a lot of cycles)
+		Main_Temperature_Outside = TemperatureGetCelsiusValue(TEMPERATURE_ID_OUTSIDE);
+		Main_Temperature_Radiator_Start = TemperatureGetCelsiusValue(TEMPERATURE_ID_RADIATOR_START);
+		Main_Temperature_Radiator_Return = TemperatureGetCelsiusValue(TEMPERATURE_ID_RADIATOR_RETURN);
 		
-		// TEST
-		if (a)
+		// Make the mixing valve moves
+		MixingValveTask();
+		
+		// Tell that controller is still alive
+		if (Is_Status_Led_On)
 		{
 			LedTurnOn(LED_ID_STATUS);
-			a = 0;
+			Is_Status_Led_On = 0;
 		}
 		else
 		{
 			LedTurnOff(LED_ID_STATUS);
-			a = 1;
+			Is_Status_Led_On = 1;
 		}
+		
+		// This is a slow regulation process, we can safely wait some time between each loop
+		_delay_ms(1000);
 	}
 }
