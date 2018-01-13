42b996c1de8564c5eb2c50439b6d41beccbedcf6
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2018-01-13 23:46:57 +0100

    Swapped pump and gas burner relays to be conform to board final connection.

--- Software/Microcontroller_Firmware/Includes/Relay.h
+++ Software/Microcontroller_Firmware/Includes/Relay.h
@@ -13,8 +13,8 @@ typedef enum
 {
 	RELAY_ID_MIXING_VALVE_LEFT = 4, //!< Connected to PD4 pin.
 	RELAY_ID_MIXING_VALVE_RIGHT = 5, //!< Connected to PD5 pin.
-	RELAY_ID_GAS_BURNER = 6, //!< Connected to PD6 pin.
-	RELAY_ID_PUMP = 7 //!< Connected to PD7 pin.
+	RELAY_ID_PUMP = 6, //!< Connected to PD6 pin.
+	RELAY_ID_GAS_BURNER = 7 //!< Connected to PD7 pin.
 } TRelayID;
 
 //-------------------------------------------------------------------------------------------------
