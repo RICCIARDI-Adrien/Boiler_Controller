333d3c55037307ea547a7f752fda2cfedb7ca46c
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-10-04 18:40:42 +0200

    Added web server monitoring page logic.

--- Software/Web_Server/Sources/Page_Monitoring.c
+++ Software/Web_Server/Sources/Page_Monitoring.c
@@ -12,7 +12,7 @@
 //-------------------------------------------------------------------------------------------------
 // Public functions
 //-------------------------------------------------------------------------------------------------
-int PageMonitoring(struct MHD_Connection *Pointer_Connection, char *Pointer_String_Response)
+int PageMonitoring(struct MHD_Connection __attribute__((unused)) *Pointer_Connection, char *Pointer_String_Response)
 {
 	int Outside_Temperature, Radiator_Start_Water_Temperature, Target_Radiator_Start_Water_Temperature, Heating_Curve_Coefficient, Heating_Curve_Parallel_Shift, Has_Error_Occurred = 0;
 	
@@ -56,15 +56,44 @@ int PageMonitoring(struct MHD_Connection *Pointer_Connection, char *Pointer_Stri
 		"	<head>\n"
 		"		<title>Chaudi&egrave;re - Monitoring</title>\n"
 		"		<meta charset=\"utf-8\" />\n"
+		"		<meta http-equiv=\"refresh\" content=\"10\">\n"
 		"	</head>\n"
 		"\n"
 		"	<body>\n"
-		"		<center>\n"
 		"		<h1>Monitoring des capteurs</h1>\n"
+		"		<p><b>Cette page se rafra&icirc;chit automatiquement toutes les 10 secondes.</b></p>\n"
 		"\n"
-		"		<p><b>TEST</b></p>\n"
+		"		<h3>Valeur des capteurs</h3>\n"
+		"		<table >\n"
+		"			<tr>\n"
+		"				<td>Temp&eacute;rature ext&eacute;rieure :</td>\n"
+		"				<td>%d°C</td>\n"
+		"			</tr>\n"
+		"			<tr>\n"
+		"				<td>Temp&eacute;rature de d&eacute;part :</td>\n"
+		"				<td>%d°C</td>\n"
+		"			</tr>\n"
+		"			<tr>\n"
+		"				<td>Temp&eacute;rature d'eau sortie chaudi&egrave;re :</td>\n"
+		"				<td>%d°C</td>\n"
+		"			</tr>\n"
+		"			<tr>\n"
+		"				<td>Coefficient de la courbe de chauffe :</td>\n"
+		"				<td>%0.1f</td>\n"
+		"			</tr>\n"
+		"			<tr>\n"
+		"				<td>D&eacute;placement parall&egrave;le de la courbe de chauffe :</td>\n"
+		"				<td>%d</td>\n"
+		"			</tr>\n"
+		"		</table>\n"
+		"\n"
+		"		<center>\n"
+		"			<p>\n"
+		"				<a href=\"/index.html\">Retour</a>\n"
+		"			</p>\n"
+		"		</center>\n"
 		"	</body>\n"
-		"</html>\n");
+		"</html>\n", Outside_Temperature, Radiator_Start_Water_Temperature, Target_Radiator_Start_Water_Temperature, Heating_Curve_Coefficient / 10.f, Heating_Curve_Parallel_Shift / 10);
 	
 	return 0;
 }
